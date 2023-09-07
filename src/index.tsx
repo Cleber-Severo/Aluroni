7b9c24165df0828b7ba3735afd07b45efad20157
Cléber Severo <clebersevero1998@gmail.com>
2023-09-07 18:49:31 -0300

    feat: installing sass and css Modules

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Cardapio from './pages/Cardapio';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <h1>hello</h1>
+    <Cardapio />
   </React.StrictMode>
 );
 
