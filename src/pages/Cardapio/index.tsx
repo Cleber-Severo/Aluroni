61b54d0dbe3dc524275c9cb792114433763564e2
Cléber Severo <clebersevero1998@gmail.com>
2023-09-08 11:42:30 -0300

    feat: Filter component

--- src/pages/Cardapio/index.tsx
+++ src/pages/Cardapio/index.tsx
@@ -2,9 +2,10 @@ import {useState} from 'react'
 import Buscador from './Buscador'
 import styles from './Cardapio.module.scss'
 import {ReactComponent as Logo } from 'assets/logo.svg'
+import Filtros from './Filtros'
 
 export default function Cardapio() {
-  const [busca, setBusca] = useState("")
+  const [busca, setBusca] = useState("");
 
   return (
    <main>
@@ -19,6 +20,9 @@ export default function Cardapio() {
     <section className={styles.cardapio}>
       <h3 className={styles.cardapio__titulo}>Cardápio</h3>
       <Buscador busca={busca} setBusca={setBusca} />
+      <div className={styles.cardapio__filtros}>
+        <Filtros />
+      </div>
     </section>
    </main>
   )
