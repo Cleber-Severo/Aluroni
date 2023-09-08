aa08ddbddc43ad391a3a2458b44d3461532b163d
Cléber Severo <clebersevero1998@gmail.com>
2023-09-08 12:49:34 -0300

    feat: Ordenador states to select an option and onblur to close when click outside of the compoenet

--- src/pages/Cardapio/Ordenador/index.tsx
+++ src/pages/Cardapio/Ordenador/index.tsx
@@ -1,18 +1,49 @@
-import React from 'react'
+import React, {useState} from 'react'
+import classNames from 'classnames'
 import styles from './Ordenador.module.scss'
 import opcoes from './opcoes.json'
+import { MdKeyboardArrowUp, MdKeyboardArrowDown } from "react-icons/md";
 
-export default function Ordenador() {
+interface OrdenadorProps {
+    ordenador: string
+    setOrdenador: React.Dispatch<React.SetStateAction<string>>
+}
+
+export default function Ordenador({ordenador, setOrdenador}: OrdenadorProps) {
+    const [aberto, setAberto] = useState(false)
+    const nomeOrdenador = ordenador && opcoes.find( opcao => opcao.value === ordenador )?.nome
+
   return (
-    <button className={styles.ordenador}>
-        <span>Ordenar Por</span>
-        <div className={styles.ordenador__options}>
-            {opcoes.map(opcao => 
-                <div className={styles.ordenador__option} key={opcao.value}>
-                    {opcao.nome}
-                </div>
-            )}
-        </div>
+    <button
+      className={classNames({
+        [styles.ordenador]: true,
+        [styles["ordenador--ativo"]]: ordenador !== "",
+      })}
+      onClick={() => setAberto(!aberto)}
+      onBlur={() => setAberto(false)}
+    >
+      <span>{nomeOrdenador || "Ordenar Por"}</span>
+      {aberto ? (
+        <MdKeyboardArrowUp size={20} />
+      ) : (
+        <MdKeyboardArrowDown size={20} />
+      )}
+      <div
+        className={classNames({
+          [styles.ordenador__options]: true,
+          [styles["ordenador__options--ativo"]]: aberto,
+        })}
+      >
+        {opcoes.map((opcao) => (
+          <div
+            className={styles.ordenador__option}
+            key={opcao.value}
+            onClick={() => setOrdenador(opcao.value)}
+          >
+            {opcao.nome}
+          </div>
+        ))}
+      </div>
     </button>
-  )
+  );
 }
