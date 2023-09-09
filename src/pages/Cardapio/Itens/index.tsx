482961568c927a2a14d8092bf192d7a741bd6769
Cléber Severo <clebersevero1998@gmail.com>
2023-09-09 15:49:55 -0300

    feat: filter functions

--- src/pages/Cardapio/Itens/index.tsx
+++ src/pages/Cardapio/Itens/index.tsx
@@ -1,14 +1,37 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import cardapio from './itens.json'
 import styles from './Itens.module.scss'
 import Item from './Item'
 
+interface ItensProps {
+  busca: string,
+  filtro: number | null,
+  ordenador: string
+}
+
+export default function Itens({busca, filtro, ordenador}: ItensProps) {
+  const [lista, setLista] = useState(cardapio)
 
 
-export default function Itens() {
+  function testaBusca(title:string) {
+    const regex = new RegExp(busca, "i");
+    return regex.test(title)
+  }
+
+  function testaFiltro(id: number) {
+    if(filtro !== null) return filtro === id;
+    return true
+  }
+
+
+  useEffect(() => {
+    const novaLista = cardapio.filter( item => testaBusca(item.title) && testaFiltro(item.category.id))
+    setLista(novaLista)
+  }, [busca, filtro])
+
   return (
     <div className={styles.itens}>
-        {cardapio.map(item => (
+        {lista.map(item => (
             <div key={item.id}>
                 <Item key={item.id} {...item}/>
             </div>
